3f1737f2175f94bd8617dc57df201decf8cbfca6
dae_ss <phanachoned@gmail.com>
2024-06-10 21:53:34 -0700

    adding oop user programming demo to week2 assignments

---
+++ ASSIGNMENTS/WEEK2/oop-app-users-i/User.py
@@ -0,0 +1,25 @@
+# your User class goes here
+
+#these classes will be a blueprint of holding data and method
+# instance variables
+class User:
+    def __init__(self, name, email, drivers_license=None): #we can make an argument to be optional by setting a default value
+        # instance variables aka instance attributes when creating a new object
+        self.name = name
+        self.email = email
+        self.drivers_license = drivers_license
+
+        pass
+    
+    def __str__(self) -> str: #dunder str method. Special behavior that allows a reader friendly version of the class object
+        return f"{self.name}, {self.email}, {self.drivers_license}"
+
+
+
+user1 = User("Daniel", "phanachoned@gmail.com", "Y7076012")
+user2 = User("Len", "i-code-in-c+@gmail.com", "SADI134")
+user = User("Conner", "onePunchMan@gmail.com", "123FJFA")
+
+print(user1)
+print(user2)
+print(user)
