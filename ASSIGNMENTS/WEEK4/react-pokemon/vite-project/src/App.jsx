b7bfefbebaf2210fd61d609b6acbefde1cf85188
dae_ss <phanachoned@gmail.com>
2024-06-27 14:51:05 -0700

    Adding react-pokemon

---
+++ ASSIGNMENTS/WEEK4/react-pokemon/vite-project/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import PokeButton from './componenets/PokeButton'
+
+function App() {
+  return (
+    <>
+      <p>Len and Rachel PokeGals!!!</p>
+      <PokeButton></PokeButton>
+    </>
+  )
+}
+
+export default App
+
+
+/**
+ * one page
+ * import pokemon font
+ * fetch api call from pokeapi grabbing the first pokemon type, then generating the next 5 pkmn similar with the same type
+ * display indivdual name on the bottom 
+ * display pokemon type as part of the title
+ * tailwind - make image grow when hovered
+ * 2 rows of 3 displayed
+ * button [type, setType] = useState ("
+ * )- create useState 
+ * 
+ * 
+ **/
---
+++ ASSIGNMENTS/WEEK4/react-pokemon/vite-project/src/componenets/PokeButton.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState }  from "react";
+import axios from "axios"
+
+    function PokeButton() {
+        
+      const [type, setType] = useState("");
+      const [pokeImg, setPokeImg] = useState("")
+      const catchEmAll = []
+
+      
+        
+      // const getPokemon = async () => { /* async will wait until API is called*/
+      //   try {                         /* the response variable will have the fetched api  */
+      //     let response = await axios.get("https://pokeapi.co/api/v2/pokemon/charizard");
+      //     (setType(response))
+      //     console.log(response)
+          
+      //   } catch (error) {
+      //     console.error("An error occurred:", error)
+      //   }
+        
+      // }
+
+
+      /* as soon as you hit NPM RUN DEV, it will IMMEDIATELY FETCH THE IMAGE! BEHIND THE CURTAIN! */
+      /* ON CLICK, YOU WILL CALL ANOTHER FUNCTION THAT WILL REVEAL THE API CALL */
+      /* using useEffect to fetch api to set the pokemon Type  */
+      useEffect( () => { /* runs on render*/ /* len "i will give you this 5$ rachel, idk when. "*/
+        async function FetchData(){
+          try {
+            let randomNum = Math.floor(Math.random()*100)
+            let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+
+            console.log(response);
+            setPokeImg(response.data.sprites.front_default)
+          } catch (error) {
+            console.error("An error occurred:", error);
+          }
+        } FetchData();
+      }, [])
+      
+      const curtainReveal = () => {
+        let ImgElement = document.getElementById("insertPokemon")
+        ImgElement.src = pokeImg
+        // let container = document.getElementById("container");
+        // container.innerHTML = '<img src={pokeImg}></img>';
+        // let ElementImg = document.createElement('img')
+       
+        // ElementImg.src = pokeImg //link
+        //container.append(ElementImg) //link
+      }
+    
+    return (
+      <>
+        <button onClick={curtainReveal}>Press Me</button>
+        {/* <div id="container"></div> */}
+        <br></br>
+        <img id = 'insertPokemon'></img>
+      </>
+    );
+
+    }
+    
+export default PokeButton;
